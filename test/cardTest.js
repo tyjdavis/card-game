46cd2171270d9dde0c715b2b57fb8d9e0618f8ae
Tyler Davis <tyjdavis@outlook.com>
2017-04-10 13:29:30 -0400

    updating

--- test/cardTest.js
+++ test/cardTest.js
@@ -15,7 +15,35 @@ describe('Card', function() {
   describe('#toString()', function() {
     it('should return a string containing suit and value', function() {
       let card = new Card(3, 'Clubs');
+      assert.equal('three of Clubs', card.toString());
+    });
+    it('should return a string for face cards too', function () {
+      let queen = new Card('Queen', 'Hearts');
+      assert.equal('Queen of Hearts', queen.toString());
+    })
+  });
+});
+
+/*
+var assert = require('assert');
+let Card = require('../lib/Card');
+
+describe('Card', function() {
+  it('has a suit', function () {
+    let card = new Card(3, 'Clubs');
+    assert.equal('Clubs', card.suit);
+  });
+
+  it('has a value', function () {
+    let card = new Card(3, 'Clubs');
+    assert.equal(3, card.value);
+  });
+
+  describe('#toString()', function() {
+    it('should return a string containing suit and value', function() {
+      let card = new Card(3, 'Clubs');
       assert.equal('3 of Clubs', card.toString());
     });
   });
 });
+*/
