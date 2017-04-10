f099e9e950a65362ed8f979ad2de2b2852488132
Tyler Davis <tyjdavis@outlook.com>
2017-04-10 15:09:10 -0400

    fixed card.js to pass test

--- test/cardTest.js
+++ test/cardTest.js
@@ -1,12 +1,13 @@
 var assert = require('assert');
 let Card = require('../lib/Card');
 
+
 describe('Card', function() {
   it('has a suit', function () {
     let card = new Card(3, 'Clubs');
     assert.equal('Clubs', card.suit);
   });
-
+  
   it('has a value', function () {
     let card = new Card(3, 'Clubs');
     assert.equal(3, card.value);
@@ -23,27 +24,3 @@ describe('Card', function() {
     })
   });
 });
-
-/*
-var assert = require('assert');
-let Card = require('../lib/Card');
-
-describe('Card', function() {
-  it('has a suit', function () {
-    let card = new Card(3, 'Clubs');
-    assert.equal('Clubs', card.suit);
-  });
-
-  it('has a value', function () {
-    let card = new Card(3, 'Clubs');
-    assert.equal(3, card.value);
-  });
-
-  describe('#toString()', function() {
-    it('should return a string containing suit and value', function() {
-      let card = new Card(3, 'Clubs');
-      assert.equal('3 of Clubs', card.toString());
-    });
-  });
-});
-*/
